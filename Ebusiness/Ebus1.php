a4c4043e492a43dc2813a375026e3d25b2fb6bba
Fergal O Connor <117333926@umail.ucc.ie>
2018-02-01 11:01:11 +0000

    project/ebus

---
+++ Ebusiness/Ebus1.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Select Product</title>
+        
+        <!--jQuery-->
+        <script src ="https://ajax.googleapps.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src ="text/javascript" src ="cost_calc.js"></script>
+        
+    </head>
+    <body>
+        <h4> Select a product</h4>
+        
+        <br/>
+        
+        <form method="POST" action="Ebus2.php">
+        
+        
+        <label for="salesforce">
+        <input type="radio" id="aws" name="product" checked onClick="disablebtnProceed()"/>
+         Salesforce @ $100
+        </Label>
+        
+        <br/>
+        
+        <label for="aws">
+            <input type ="radio" id="aws" name="product" onCLick="disablebtnProceed()"/>
+            AWS @ $300
+        </Label>
+           
+           
+            <br/>
+            <br/>
+            
+        <Label for="subtotal">
+            Sub Total
+            <input type="text" id="subtotal" value="0.00" readonly/>
+        </Label>
+            
+            <br/>
+            
+        <Label for="total">
+            Total
+            <input type="text" id="total" value="0.00" readonly/>
+        </Label>    
+            
+        <br/>    
+        <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
+            
+        </form>
+        
+        <br/>
+        
+        <button onClick="calSub()">Calculate Cost</button>
+        
+        <br/>
+        
+        <a role="button" href="Ebus1.php">Clear Choice</a>
+         
+        
+    </body>
+    
+    
+</html>
