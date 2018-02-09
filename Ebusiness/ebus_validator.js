5ca20093d8b6af32466737ff2daa9f4673faefcf
Fergal O Connor <117333926@umail.ucc.ie>
2018-02-09 11:00:55 +0000

    l

---
+++ Ebusiness/ebus_validator.js
@@ -0,0 +1,21 @@
+/* global $ */
+
+function validateDetails(){
+    
+    var pin;
+    
+    pin = document.getElementById("user_pin").value;
+    
+    if (pin==""){
+        alert("Please enter your PIN");
+        }
+    else if (String(pin).length<4){
+        alert("Please make sure your PIN is accurate");
+        }
+    else{
+        enablebtnPurchase();
+        }
+}
+
+function enablebtnPurchase(){
+
